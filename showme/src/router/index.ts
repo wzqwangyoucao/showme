150f19eb86a9470e158c3798e8a03244659db275
wzqwangyoucao <1737481406@qq.com>
2021-02-22 20:35:40 +0800

    新增路由

---
+++ showme/src/router/index.ts
@@ -0,0 +1,10 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import routes from './router'
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router
+
