8a73ef58f41f4530631030fd985a5574b9e03aed
wzqwangyoucao <1737481406@qq.com>
2021-02-23 09:41:53 +0800

    router增加ts类型注解

--- showme/src/router/router.ts
+++ showme/src/router/router.ts
@@ -1,3 +1,7 @@
-export default [
+import { RouteRecordRaw } from 'vue-router'
+
+const route: Array<RouteRecordRaw> = [
     { path: '/', component: () => import('../components/HelloWorld.vue') },
 ]
+
+export default route
