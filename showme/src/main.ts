bcf9d51a225f2312d26b41e2175b357aa2fb6712
wzqwangyoucao <1737481406@qq.com>
2021-02-23 11:59:56 +0800

    增加login路由

--- showme/src/main.ts
+++ showme/src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import Name from './1.name'
+import router from './router'
 
-console.log(App);
 console.log(Name.name);
 
-import router from './router'
 
 createApp(App).use(router).mount('#app')
---
+++ showme/src/page/login.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- <form action=""> -->
+        账号：<input type="text" v-model="userName">
+        密码：<input type="text" v-model="password">
+        <button @click="login">登录</button>
+    <!-- </form> -->
+</template>
+<script lang="ts">
+interface userInfo {
+    userName: string,
+    password: string
+}
+import { defineComponent, reactive, toRefs } from "vue";
+export default defineComponent({
+    setup() {
+        let userInfo = reactive<userInfo>({
+            userName: '',
+            password: ''
+        })
+
+        function login () {
+            console.log(userInfo.userName, userInfo.password);
+        }
+
+        return {...toRefs(userInfo), login}
+    },
+    methods: {}
+});
+</script>
