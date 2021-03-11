1977ab76b77d4ffbe81ecae78f129959ecc7a070
wzqwangyoucao <1737481406@qq.com>
2021-03-11 16:48:55 +0800

    链接数据库

--- showmeEnd/app/config.js
+++ showmeEnd/app/config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // connectionStr: 'mongodb+srv://wangzhiqin15:<password>@cluster0.7htlt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    connectionStr: 'mongodb+srv://wangzhiqin15:Wang525715@cluster0.7htlt.mongodb.net/Cluster0?retryWrites=true&w=majority'
+    connectionStr: 'mongodb+srv://wangzqtest:wangzqtest@cluster0.tpslk.mongodb.net/Cluster0?retryWrites=true&w=majority'
 }
--- showmeEnd/app/model/user.js
+++ showmeEnd/app/model/user.js
@@ -3,8 +3,9 @@ const {
 } = require('mongoose')
 
 const userInfoSchema = new Schema({
+    __v: { type: Number, select: false },
     userName: { type: String, require: true },
-    password: { type: String, require: true },
+    password: { type: String, require: true, select: false }
 });
 
 module.exports = model('userInfooModel', userInfoSchema);
